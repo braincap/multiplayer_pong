79b56e45012a7262c23a840439d454d461e5d7e3
Jai Tiwari <contactjaitiwari@gmail.com>
2017-04-16 17:24:50 -0400

    local works

---
+++ public/javascript/index.js
@@ -0,0 +1,160 @@
+// Socket handling
+var socket = io();
+
+// Send partner's ID
+var partnerId = prompt("Enter your friend's ID");
+
+var request = new XMLHttpRequest();
+request.open('GET', '/' + partnerId, true);
+request.onload = function () {
+  if (request.status >= 200 && request.status < 400) {
+    console.log("Friend ID sent");
+    var resp = request.responseText;
+  } else {
+    console.log("Server connected but error returned");
+  }
+};
+
+request.onerror = function () {
+  console.log("Error while connecting to server");
+};
+
+request.send();
+
+// Canvas and rest of the game
+
+var canvas = document.getElementById('myCanvas');
+var c = canvas.getContext('2d');
+
+var paddleWidth = 15;
+var paddleHeight = 90;
+var paddleAX = 0;
+var paddleAY = (canvas.height - paddleHeight) / 2;
+var paddleBX = (canvas.width - paddleWidth);
+var paddleBY = (canvas.height - paddleHeight) / 2;
+var greenUpPressed = false;
+var greenDownPressed = false;
+var redUpPressed = false;
+var redDownPressed = false;
+
+var ballX = canvas.width / 2;
+var ballY = canvas.height / 2;
+var dx = 5;
+var dy = 5;
+var ballRadius = 10;
+
+var redColor = "#FF1744"
+var greenColor = "#00BFA5"
+var ballColor = "#0095DD"
+
+window.addEventListener('keydown', keyDownHandler, false);
+window.addEventListener('keyup', keUpHandler, false);
+socket.on('redUpPressed', function (msg) {
+  redUpPressed = msg;
+  console.log("Friend's press received redUpPressed : " + redUpPressed);
+});
+socket.on('redDownPressed', function (msg) {
+  redDownPressed = msg;
+  console.log("Friend's press received : " + redDownPressed);
+});
+
+
+function keyDownHandler(e) {
+  if (e.keyCode == 40) {
+    greenUpPressed = true;
+    socket.emit('redDownPressed', true);
+  } else if (e.keyCode == 38) {
+    greenDownPressed = true;
+    socket.emit('redUpPressed', true);
+  }
+}
+
+function keUpHandler(e) {
+  if (e.keyCode == 40) {
+    greenUpPressed = false;
+    socket.emit('redDownPressed', false);
+  } else if (e.keyCode == 38) {
+    greenDownPressed = false;
+    socket.emit('redUpPressed', false);
+  }
+}
+
+function drawPaddleA() {
+  c.beginPath();
+  c.rect(paddleAX, paddleAY, paddleWidth, paddleHeight);
+  c.fillStyle = greenColor;
+  c.strokeStyle
+  c.fill();
+  c.closePath();
+  c.beginPath();
+  c.moveTo(paddleAX + paddleWidth, paddleAY);
+  c.lineTo(paddleAX + paddleWidth, paddleAY + paddleHeight);
+  c.stroke();
+  c.closePath();
+}
+
+function drawPaddleB() {
+  c.beginPath();
+  c.rect(paddleBX, paddleBY, paddleWidth, paddleHeight);
+  c.fillStyle = redColor;
+  c.fill();
+  c.closePath();
+  c.beginPath();
+  c.moveTo(paddleBX, paddleBY);
+  c.lineTo(paddleBX, paddleBY + paddleHeight);
+  c.stroke();
+  c.closePath();
+}
+
+function drawBall() {
+  c.beginPath();
+  c.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+  c.fillStyle = ballColor;
+  c.fill();
+  c.closePath();
+}
+
+function draw() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  drawPaddleA();
+  drawPaddleB();
+  drawBall();
+  bounceCheck();
+  ballX = ballX + dx;
+  ballY = ballY + dy;
+  //Left paddle USER
+  if (greenUpPressed) {
+    paddleAY += 7;
+  } else if (greenDownPressed) {
+    paddleAY -= 7;
+  }
+  //Right paddle SOCKET
+  if (redUpPressed) {
+    paddleBY += 7;
+  } else if (redDownPressed) {
+    paddleBY -= 7;
+  }
+
+
+  requestAnimationFrame(draw);
+}
+
+function bounceCheck() {
+  if (ballY + dy + ballRadius >= canvas.height || ballY + dy - ballRadius <= 0) {     //vertical wall bounce check
+    dy = -dy;
+  } else if (ballX + dx + ballRadius >= canvas.width - paddleWidth && ballY >= paddleBY && ballY <= paddleBY + paddleHeight) {   //right B pad played
+    dx = -dx;
+  } else if (ballX + dx - ballRadius <= paddleWidth && ballY >= paddleAY && ballY <= paddleAY + paddleHeight) {    //left A pad played
+    dx = -dx;
+  } else if (ballX + ballRadius >= paddleBX && ballX - ballRadius <= canvas.width && ballY + ballRadius >= paddleBY && ballY - ballRadius <= paddleBY + paddleHeight) {
+    dy = -dy;
+  } else if (ballX - ballRadius <= paddleWidth && ballX + ballRadius >= 0 && ballY + ballRadius >= paddleAY && ballY - ballRadius <= paddleAY + paddleHeight) {
+    dy = -dy;
+  } else if (ballX + dx + ballRadius >= canvas.width) {
+    // window.location.reload();
+  } else if (ballX + dx - ballRadius <= 0) {
+    // window.location.reload();
+  }
+}
+
+draw();
--- server.js
+++ server.js
@@ -3,17 +3,34 @@ var path = require('path');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var ownId, friendId;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/:userInput', (req, res) => {
+  friendId = req.params.userInput;
+  console.log("Friend ID received and stored : " + friendId);
+});
+
 io.on('connection', function (socket) {
   console.log('a user connected');
-  socket.on('chat message', function (msg) {
-    io.emit('chat message', msg);
+  ownId = socket.id;
+
+  console.log("Own id : " + ownId);
+  console.log("Friend id : " + friendId);
+
+  socket.on('redUpPressed', function (msg) {
+    io.to(friendId).emit('redUpPressed', msg);
   });
+
+  socket.on('redDownPressed', function (msg) {
+    io.to(friendId).emit('redDownPressed', msg);
+  });
+
   socket.on('disconnect', function () {
     console.log('user disconnected');
-  })
+  });
 });
+
 
 http.listen(process.env.PORT || 3000, () => console.log(`listening on *:${process.env.PORT || 3000}`));
